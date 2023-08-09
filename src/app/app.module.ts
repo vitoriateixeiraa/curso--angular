350876cb73480bb16fdc17339cd71fd20348689f
vitoriateixeiraa <teixeiravitoria057@gmail.com>
2023-08-09 16:23:29 -0300

    firts commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { IdRenderComponent } from './components/if-render/id-render.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    ParentDataComponent,
+    IdRenderComponent,
+    IfRenderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent implements OnInit {
+  name: string = 'Vitoria';
+  age: number = 21;
+
+  constructor() { }
+  ngOnInit(): void {
+  }
+}
